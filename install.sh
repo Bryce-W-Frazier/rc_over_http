b2b203da597f0a740c4cb61a2153ff13c9de3102
Bryce W. Frazier <brycewill.id@gmail.com>
2026-01-31 22:10:39 -0900

    making install.sh work locally on server

--- install.sh
+++ install.sh
@@ -2,13 +2,10 @@
 
 # export_to_server.sh
 # Move software to Rasberry Pi
-# Run in root of git repo
+# Run in root of git repo on server
+# Requires root access
 # Bryce W. Frazier
 # Started: 2026-01-31
 
-read -p "Server Address: " address
-read -sp "Server password: " password
 
-scp server/* $address:~/server
-ssh $address 'sudo mv server/* /var/node'
-
+sudo mv server/* /var/node
