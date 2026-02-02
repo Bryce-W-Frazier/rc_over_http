0476b3280d4986cfaae130fccd79e2ece367321e
Bryce W. Frazier <brycewill.id@gmail.com>
2026-02-01 15:52:43 -0900

    now goes straight, when no steering input

--- server/client/main.js
+++ server/client/main.js
@@ -3,14 +3,38 @@
 // Bryce W. Frazier
 // Started: 2026-01-31
 
+// Open Web Socket With Werver
 const socket = io();
 
+// RC Control vars
+let steer_vector = 0;
+
+// Calculate User Input Vectors
 window.addEventListener('keydown', () => {
   if (event.repeat) return;
   
   if (event.key == 'd') {
     socket.emit('steer-to', -500);
-  } else if (event.key == 'a') {
+  } 
+  if (event.key == 'a') {
     socket.emit('steer-to', 500);
   }
+
+  socket.emit(steer-to, steer_vector);
 });
+
+window.addEventListener('keyup', () => {
+  if (event.repeat) return;
+
+  if (event.key == 'a' || event.key == 'd') {
+    socket.emit('steer-to', 0);
+  }
+});
+
+// Send Input Vectors to Server
+/*setInterval(() => {
+  socket.emit('steer-to', steer_vector);
+
+  // Reset Input Vectors
+  steer_vector = 0;
+}, 50);*/
